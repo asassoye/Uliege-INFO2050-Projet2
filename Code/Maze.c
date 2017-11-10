c5cc5293242fc9209b58c62c8bed09fdf997ed59
Noémie <noemielecocq@hotmail.com>
2017-11-10 16:39:08 +0100

    returns

--- Code/Maze.c
+++ Code/Maze.c
@@ -2,34 +2,31 @@
 
 Maze* mzCreate(size_t size){
 
+    return NULL;
 }
 
-void mzFree(Maze* maze){
-
-}
-
-
-size_t mzSize(const Maze* maze){
+void mzFree(Maze* maze) {
 
 }
 
+size_t mzSize(const Maze* maze) {
 
+    return 0;
+}
 
 bool mzIsWallClosed(Maze* maze, Coord cell1, Coord cell2){
 
+    return 0;
 }
-
 
 void mzSetWall(Maze* maze, Coord cell1, Coord cell2, bool close){
 
 }
 
-
-
 bool mzIsValid(const Maze* maze){
 
+    return 0;
 }
-
 
 void mzPrint(const Maze* maze, FILE* out){
 
