fa38349379ba86096382831b30a7983f3d5cdea1
Andrew Sassoye <andrew.sassoye@student.ulg.ac.be>
2017-11-10 16:38:03 +0100

    Creations des fonctions vide dans maze.c

--- Code/Maze.c
+++ Code/Maze.c
@@ -1,2 +1,36 @@
 #include "Maze.h"
 
+Maze* mzCreate(size_t size){
+
+}
+
+void mzFree(Maze* maze){
+
+}
+
+
+size_t mzSize(const Maze* maze){
+
+}
+
+
+
+bool mzIsWallClosed(Maze* maze, Coord cell1, Coord cell2){
+
+}
+
+
+void mzSetWall(Maze* maze, Coord cell1, Coord cell2, bool close){
+
+}
+
+
+
+bool mzIsValid(const Maze* maze){
+
+}
+
+
+void mzPrint(const Maze* maze, FILE* out){
+
+}
