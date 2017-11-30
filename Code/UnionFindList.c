762cc11c5bf416c4dd0be2faa533ec229301fc07
Noémie <noemielecocq@hotmail.com>
2017-11-30 16:06:48 +0100

    ufUnion

--- Code/UnionFindList.c
+++ Code/UnionFindList.c
@@ -78,12 +78,12 @@ size_t ufComponentsCount(const UnionFind *union_find) {
     return count;
 }
 
-void ufFree(UnionFind *union_find){
+void ufFree(UnionFind *union_find) {
     Node *actual = union_find->first;
 
     Node *next = actual->next;
 
-    while(next != NULL){
+    while (next != NULL) {
         setFree(actual->set);
         free(actual);
         actual = next;
@@ -97,14 +97,14 @@ void ufFree(UnionFind *union_find){
 
 }
 
-static void setFree(Set *set){
+static void setFree(Set *set) {
     assert(set != NULL);
 
     Cell *actual = set->head;
 
     Cell *next = actual->next;
 
-    while(set->size > 1){
+    while (set->size > 1) {
         free(actual);
         actual = next;
 
@@ -114,9 +114,74 @@ static void setFree(Set *set){
     }
 
     free(actual);
-
     free(set);
 }
+
+static ufStatus Union(UnionFind *unionFind, Set *set1, Set *set2) {
+    Cell *tmp;
+    set1->size += set2->size; //update the size of the set
+
+    //update the tail and head pointers
+    tmp = set1->tail;
+    set1->tail = set2->tail;
+    tmp->next = set2->head;
+
+    //update the set pointer in each element of the second set
+    tmp = set2->head;
+    while (tmp != NULL) {
+        tmp->set = set1;
+        tmp = tmp->next;
+    }
+
+    //update the linked list of sets
+    Node *node = unionFind->first;
+    Node *prev = node;
+    while (node != NULL) {
+        if (node->set == set2) {
+            if (node == unionFind->first) {
+                unionFind->first = node->next;
+            } else {
+                prev->next = node->next;
+            }
+            free(node);
+            return UF_MERGED;
+        }
+        prev = node;
+        node = node->next;
+    }
+    return UF_ERROR;
+}
+
+
+ufStatus ufUnion(UnionFind *union_find, size_t item1, size_t item2) {
+    size_t it1 = ufFind(union_find, item1);
+    size_t it2 = ufFind(union_find, item2);
+    if (it1 == it2)
+        return UF_SAME;
+    Set *set1 = NULL;
+    Set *set2 = NULL;
+    Node *node = union_find->first;
+    while (node != NULL) {
+        if (node->set->head->key == it1) {
+            set1 = node->set;
+        }
+        if (node->set->head->key == it2) {
+            set2 = node->set;
+        }
+        if (set1 != NULL && set2 != NULL)
+            break;
+        node = node->next;
+    }
+    if (set1 == NULL || set2 == NULL)
+        return UF_ERROR;
+
+    //concatenate the two sets, puts the smaller one after the bigger one to reduce cost
+    if (set1->size >= set2->size)
+        return Union(union_find, set1, set2);
+    else
+        return Union(union_find, set2, set1);
+}
+
 
 size_t ufFind(const UnionFind *union_find, size_t item) {
     Node *node = union_find->first;
@@ -133,4 +198,21 @@ size_t ufFind(const UnionFind *union_find, size_t item) {
         node = node->next;
     }
     return (size_t) -1;
+}
+
+//temporary function to print uf
+void ufPrint(UnionFind *unionFind) {
+    Node *node = unionFind->first;
+    Cell *cell;
+    while (node != NULL) {
+        printf("(");
+        cell = node->set->head;
+        while (cell != NULL) {
+            printf("%zu", cell->key);
+            cell = cell->next;
+        }
+        printf(")");
+        node = node->next;
+    }
+    printf("\n");
 }
