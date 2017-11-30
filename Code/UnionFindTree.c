0f295597fbb24122dcc83edc0341a71f404df512
Andrew Sassoye <andrew.sassoye@student.ulg.ac.be>
2017-11-30 18:47:29 +0100

    ufunion, uffind

--- Code/UnionFindTree.c
+++ Code/UnionFindTree.c
@@ -6,30 +6,32 @@
 typedef struct node_t Node;
 
 struct node_t {
-    int key;
-    int rang;
+    size_t key;
     Node *parent;
+    Node *leftChild;
+    Node *rightChild;
 };
 
-struct union_find_t{
+struct union_find_t {
     Node **vector;
     size_t size;
 };
 
 
-UnionFind *ufCreate(size_t n_items){
+UnionFind *ufCreate(size_t n_items) {
     UnionFind *unionFind = malloc(sizeof(*unionFind));
 
-    Node **vector = malloc(n_items * sizeof(Node*));
+    Node **vector = malloc(n_items * sizeof(Node *));
 
     Node *actual = NULL;
 
-    for(unsigned int i = 0; i < n_items; i++){
+    for (unsigned int i = 0; i < n_items; i++) {
         actual = malloc(sizeof(Node));
 
         actual->key = i;
         actual->parent = NULL;
-        actual->rang = 1;
+        actual->rightChild = NULL;
+        actual->leftChild = NULL;
 
         vector[i] = actual;
     }
@@ -41,8 +43,8 @@ UnionFind *ufCreate(size_t n_items){
 
 }
 
-void ufFree(UnionFind *union_find){
-    for(unsigned int i = 0; i < union_find->size; i++){
+void ufFree(UnionFind *union_find) {
+    for (unsigned int i = 0; i < union_find->size; i++) {
         free(union_find->vector[i]);
     }
     free(union_find->vector);
@@ -50,16 +52,57 @@ void ufFree(UnionFind *union_find){
 }
 
 
-ufStatus ufUnion(UnionFind *union_find, size_t item1, size_t item2){
-    return UF_ERROR;
+ufStatus ufUnion(UnionFind *union_find, size_t item1, size_t item2) {
+    if (item1 > union_find->size || item2 > union_find->size) {
+        return UF_ERROR;
+    }
+
+    Node *element1 = union_find->vector[ufFind(union_find, item1)];
+    Node *element2 = union_find->vector[ufFind(union_find, item2)];
+
+    if (element1 == element2) {
+        return UF_SAME;
+    }
+
+    Node *y = NULL;
+    Node *x = element1;
+    Node *z = element2;
+
+    while(x != NULL){
+        y = x;
+        if(z->key < x->key){
+            x = x->leftChild;
+        } else {
+            x = x->rightChild;
+        }
+    }
+    z->parent = y;
+
+    if (y == NULL) {
+        *element1 = *z;
+    }else if (z->key < y->key){
+        y->leftChild = z;
+    }else {
+        y->rightChild = z;
+    }
+
+    return UF_MERGED;
+
 }
 
 
-size_t ufFind(const UnionFind *union_find, size_t item){
-    return 0;
+size_t ufFind(const UnionFind *union_find, size_t item) {
+    Node *element = union_find->vector[item];
+
+    while (element->parent != NULL) {
+        element = element->parent;
+    }
+
+    return element->key;
+
 }
 
-size_t ufComponentsCount(const UnionFind *union_find){
+size_t ufComponentsCount(const UnionFind *union_find) {
     return 0;
 }
 
