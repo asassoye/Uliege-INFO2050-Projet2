78af2306832d324991a7f52398befd96e094cb1c
Noémie <noemielecocq@hotmail.com>
2017-12-01 17:00:26 +0100

    maze

--- Code/Maze.c
+++ Code/Maze.c
@@ -4,20 +4,21 @@
 #include "Maze.h"
 #include "UnionFind.h"
 
+typedef struct neighbour_t Neighbour;
+
 struct maze_t {
     size_t size;
     Coord **coord;
+    Neighbour **neighbours;
+
 };
 
 
-static bool isNeighbour(size_t size, size_t cell1, size_t cell2) {
-    if ((cell2 == cell1 + 1 && cell2 % size + 1 != size) ||
-        (cell2 == cell1 - 1 || cell2 == cell1 - size
-         || cell2 == cell1 + size))
-        return true;
-    return false;
-}
-
+struct neighbour_t {
+    size_t cell1;
+    size_t cell2;
+    bool wall;
+};
 
 static void getNeighbours(size_t *tab, size_t x, size_t size) {
     if (x > size)
@@ -31,12 +32,39 @@ static void getNeighbours(size_t *tab, size_t x, size_t size) {
 }
 
 
+static void setNeighbours(Neighbour **neighbours, size_t size) {
+    int count = 0;
+    for (size_t i = 0; i < (size * size); i++) {
+        if ((i + size) < (size * size)) {
+            neighbours[count] = malloc(sizeof(Neighbour));
+            neighbours[count]->cell1 = i;
+            neighbours[count]->cell2 = i + size;
+            neighbours[count]->wall = true;
+            count++;
+        }
+        if ((i + 1) % size != 0) {
+            neighbours[count] = malloc(sizeof(Neighbour));
+            neighbours[count]->cell1 = i;
+            neighbours[count]->cell2 = i + 1;
+            neighbours[count]->wall = true;
+            count++;
+        }
+    }
+}
+
+
 Maze *mzCreate(size_t size) {
     size_t vecSize = size * size;
     srand(time(NULL));
     Maze *maze = malloc(sizeof(Maze));
     maze->size = size;
     maze->coord = malloc(vecSize * sizeof(Coord));
+    maze->neighbours = malloc(size * (size - 1) * 2 * sizeof(Neighbour));
+    setNeighbours(maze->neighbours, size);
+    for (size_t i = 0; i < (maze->size * (maze->size - 1) * 2); ++i) {
+        printf("%zu,%zu ", maze->neighbours[i]->cell1, maze->neighbours[i]->cell2);
+    }
+
     int t = 0;
     for (size_t i = 0; i < size; ++i) {
         for (size_t j = 0; j < size; ++j) {
@@ -47,17 +75,16 @@ Maze *mzCreate(size_t size) {
         }
     }
     UnionFind *unionFind = ufCreate(vecSize);
+    ufPrint(unionFind);
     size_t rand1;
-    size_t rand2 = INT_MAX;
-    size_t tab[4] = {INT_MAX};
     while (ufComponentsCount(unionFind) != 1) {
-        rand1 = rand() % (size * size);
-        getNeighbours(tab, rand1, size);
-        while (rand2 == INT_MAX)
-            rand2 = tab[rand() % (4)];
-        //mzSetWall(maze,maze->coord[rand1], maze->coord[rand2], false);
-        ufUnion(unionFind, rand1, rand2);
+        rand1 = rand() % (size * (size - 1) * 2);
+        if (maze->neighbours[rand1]->wall == true) {
+            ufUnion(unionFind, maze->neighbours[rand1]->cell1, maze->neighbours[rand1]->cell2);
+            maze->neighbours[rand1]->wall = false;
+        }
     }
+    ufPrint(unionFind);
     ufFree(unionFind);
     return maze;
 }
@@ -67,6 +94,10 @@ void mzFree(Maze *maze) {
     for (size_t i = 0; i < (maze->size * maze->size); ++i) {
         free(maze->coord[i]);
     }
+    for (size_t i = 0; i < (maze->size * (maze->size - 1) * 2); ++i) {
+        free(maze->neighbours[i]);
+    }
+    free(maze->neighbours);
     free(maze->coord);
     free(maze);
 }
